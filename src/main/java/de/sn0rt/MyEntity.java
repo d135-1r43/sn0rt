d75062bb9cd253933939af6673edef820284ce66
Markus Herhoffer <markus@herhoffer.net>
2025-10-22 17:10:07 +0200

    🎨 (format): Add formatter and format

--- src/main/java/de/sn0rt/MyEntity.java
+++ src/main/java/de/sn0rt/MyEntity.java
@@ -3,13 +3,13 @@ package de.sn0rt;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 
-
 /**
- * Example JPA entity defined as a Panache Entity.
- * An ID field of Long type is provided, if you want to define your own ID field extends <code>PanacheEntityBase</code> instead.
+ * Example JPA entity defined as a Panache Entity. An ID field of Long type is
+ * provided, if you want to define your own ID field extends
+ * <code>PanacheEntityBase</code> instead.
  *
- * This uses the active record pattern, you can also use the repository pattern instead:
- * .
+ * This uses the active record pattern, you can also use the repository pattern
+ * instead: .
  *
  * Usage (more example on the documentation)
  *
@@ -19,11 +19,10 @@ import jakarta.persistence.Entity;
  *         entity1.field = "field-1";
  *         entity1.persist();
  *
- *         List<MyEntity> entities = MyEntity.listAll();
- *     }
- * }
+ *         List<MyEntity> entities = MyEntity.listAll(); } }
  */
 @Entity
-public class MyEntity extends PanacheEntity {
-    public String field;
+public class MyEntity extends PanacheEntity
+{
+	public String field;
 }
